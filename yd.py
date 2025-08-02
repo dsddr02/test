4abc256dd5130bf60773f11e7f23c149ce736955
dsddr02 <fldz5555a@outlook.com>
2025-08-03 00:26:42 +0800

    Create yd.py

---
+++ yd.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import sys
+
+def csv_to_txt(csv_filename, output_filename, area_name):
+    df = pd.read_csv(csv_filename, encoding='utf-8')
+    ips = df.iloc[:, 0]
+    download_speeds = df.iloc[:, 5]
+
+    with open(output_filename, 'w', encoding='utf-8') as f:
+        for i, (ip, speed) in enumerate(zip(ips, download_speeds)):
+            #f.write(f"{ip}:8443#{area_name}{i+1}\n")
+            f.write(f"{ip}\n")
+def csv_to_txtt(csv_filename, output_filename, area_name):
+    df = pd.read_csv(csv_filename, encoding='utf-8')
+    ips = df.iloc[:, 0]
+    download_speeds = df.iloc[:, 5]
+
+    with open(output_filename, 'w', encoding='utf-8') as f:
+        for i, (ip, speed) in enumerate(zip(ips, download_speeds)):
+            f.write(f"{ip}:2087#{area_name}{i+1}\n")
+            
+csv_to_txt("HKG.csv", "yd.txt", "xn")
+csv_to_txtt("HKG.csv", "valid_ipsyd.txt", "xn")
