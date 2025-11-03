355ccbb5769bf51addec96ce3d8cff2e76a63a4b
dsddr02 <fldz5555a@outlook.com>
2025-11-03 15:08:37 +0800

    Update yd.py

--- yd.py
+++ yd.py
@@ -1,15 +1,17 @@
 import pandas as pd
-import sys
 
 def csv_to_txt(csv_filename, output_filename, area_name):
     df = pd.read_csv(csv_filename, encoding='utf-8')
-    ips = df.iloc[:, 0]
-    download_speeds = df.iloc[:, 5]
-
+    
+    # 按TCP延迟列排序并取前9个
+    df_sorted = df.sort_values(by=df.columns[6])  # 第7列是TCP延迟
+    top_9_ips = df_sorted.iloc[:9, 0]  # 前9行的第1列（IP地址）
+    
     with open(output_filename, 'w', encoding='utf-8') as f:
-        for i, (ip, speed) in enumerate(zip(ips, download_speeds)):
-            #f.write(f"{ip}:8443#{area_name}{i+1}\n")
+        for ip in top_9_ips:
             f.write(f"{ip}\n")
+    
+    
 def csv_to_txtt(csv_filename, output_filename, area_name):
     df = pd.read_csv(csv_filename, encoding='utf-8')
     ips = df.iloc[:, 0]
