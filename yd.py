359aa6e7933293483bf7e073f7afe2a8d4443a49
dsddr02 <fldz5555a@outlook.com>
2025-11-05 05:10:31 +0800

    Update yd.py

--- yd.py
+++ yd.py
@@ -14,12 +14,12 @@ def csv_to_txt(csv_filename, output_filename, area_name):
     
 def csv_to_txtt(csv_filename, output_filename, area_name):
     df = pd.read_csv(csv_filename, encoding='utf-8')
-    ips = df.iloc[:, 0]
-    download_speeds = df.iloc[:, 5]
-
+    df_sorted = df.sort_values(by=df.columns[6])  # 第7列是TCP延迟
+    top_9_ips = df_sorted.iloc[:9, 0]  # 前9行的第1列（IP地址）
+    
     with open(output_filename, 'w', encoding='utf-8') as f:
-        for i, (ip, speed) in enumerate(zip(ips, download_speeds)):
-            f.write(f"{ip}:2087#{area_name}{i+1}↓{speed}MB/s\n")
+        for ip in top_9_ips:
+            f.write(f"{ip}:2087#{area_name}{i+1}\n")
             
 csv_to_txt("result.csv", "yd.txt", "xn")
 csv_to_txtt("result.csv", "valid_ipsyd.txt", "xn")
